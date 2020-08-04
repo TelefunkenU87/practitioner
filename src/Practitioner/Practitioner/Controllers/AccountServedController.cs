c63373542768e9c942d5cfbabdd8877b6e23dd37
telefunk <telewebb@gmail.com>
2020-08-03 20:40:35 -0400

    Broke out ExperienceController methods into their own controllers

---
+++ src/Practitioner/Practitioner/Controllers/AccountServedController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practitioner.Models;
+using Practitioner.ViewModel;
+
+namespace Practitioner.Controllers
+{
+    public class AccountServedController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IAccountServedRepository _accountServedRepository;
+
+        public AccountServedController(IEmployeeRepository employeeRepository, IAccountServedRepository accountServedRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _accountServedRepository = accountServedRepository;
+        }
+        
+        public IActionResult AccountServed(int id)
+        {
+            var employee = _employeeRepository.GetEmployeeById(id);
+            var accountsServed = _accountServedRepository.GetAccountServedByEmployeeId(id);
+            var categoryNav = new CategoryNavViewModel { EmployeeId = employee.EmployeeId, ExperienceActive = "active" };
+            var experienceTabNav = new ExperienceTabNavViewModel { EmployeeId = employee.EmployeeId, AccountsServedActive = "active" };
+
+            return View(new AccountServedViewModel
+            {
+                Employee = employee,
+                AccountsServed = accountsServed,
+                CategoryNav = categoryNav,
+                ExperienceTabNav = experienceTabNav,
+                NewAccountServed = new AccountServed { AccountServedId = 0, EmployeeId = employee.EmployeeId }
+            });
+        }
+
+        public IActionResult AccountServedEdit(int id)
+        {
+            var accountServed = _accountServedRepository.GetAccountServedById(id);
+            //return View(accountServed);
+            return PartialView(accountServed);
+        }
+
+        [HttpPost]
+        public IActionResult AccountServedEdit(AccountServed updatedAccountServed)
+        {
+            int Id = 0;
+            if (updatedAccountServed.AccountServedId > 0)
+            {
+                _accountServedRepository.UpdateAccountServed(updatedAccountServed);
+                Id = updatedAccountServed.EmployeeId;
+            }
+            else if (ModelState.IsValid)
+            {
+                _accountServedRepository.AddAccountServed(updatedAccountServed);
+                Id = updatedAccountServed.EmployeeId;
+            }
+            if (Id != 0)
+            {
+                return RedirectToAction("AccountServed", new { id = Id });
+            }
+            else
+            {
+                return View(updatedAccountServed);
+            }
+        }
+
+        public IActionResult AccountServedDelete(int id, int employeeId)
+        {
+            int Id = employeeId;
+            if (id > 0)
+            {
+                _accountServedRepository.DeleteAccountServed(id);
+            }
+            else
+            {
+                return NotFound();
+            }
+            return RedirectToAction("AccountServed", new { id = Id });
+        }
+    }
+}
---
+++ src/Practitioner/Practitioner/Controllers/ReloInterestController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practitioner.Models;
+using Practitioner.ViewModel;
+
+namespace Practitioner.Controllers
+{
+    public class ReloInterestController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IReloInterestRepository _reloInterestRepository;
+
+        public ReloInterestController(IEmployeeRepository employeeRepository, IReloInterestRepository reloInterestRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _reloInterestRepository = reloInterestRepository;
+        }
+
+        public IActionResult ReloInterest(int id)
+        {
+            var employee = _employeeRepository.GetEmployeeById(id);
+            var reloInterests = _reloInterestRepository.GetReloInterestByEmployeeId(id);
+            var categoryNav = new CategoryNavViewModel { EmployeeId = employee.EmployeeId, InterestActive = "active" };
+            var interestTabNav = new InterestTabNavViewModel { EmployeeId = employee.EmployeeId, ReloInterestActive = "active" };
+
+            return View(new ReloInterestViewModel
+            {
+                ReloInterests = reloInterests,
+                Employee = employee,
+                CategoryNav = categoryNav,
+                InterestTabNav = interestTabNav,
+                NewReloInterest = new ReloInterest { ReloInterestId = 0, EmployeeId = employee.EmployeeId }
+            });
+        }
+
+        public IActionResult ReloInterestEdit(int id)
+        {
+            var reloInterest = _reloInterestRepository.GetReloInterestById(id);
+            return PartialView(reloInterest);
+        }
+
+        [HttpPost]
+        public IActionResult ReloInterestEdit(ReloInterest updatedReloInterest)
+        {
+            int Id = 0;
+            if (updatedReloInterest.ReloInterestId > 0)
+            {
+                _reloInterestRepository.UpdateReloInterest(updatedReloInterest);
+                Id = updatedReloInterest.EmployeeId;
+            }
+            else if (ModelState.IsValid)
+            {
+                _reloInterestRepository.AddReloInterest(updatedReloInterest);
+                Id = updatedReloInterest.EmployeeId;
+            }
+            if (Id != 0)
+            {
+                return RedirectToAction("ReloInterest", new { id = Id });
+            }
+            else
+            {
+                return View(updatedReloInterest);
+            }
+        }
+
+        public IActionResult ReloInterestDelete(int id, int employeeId)
+        {
+            int Id = employeeId;
+            if (id > 0)
+            {
+                _reloInterestRepository.DeleteReloInterest(id);
+            }
+            else
+            {
+                return NotFound();
+            }
+            return RedirectToAction("ReloInterest", new { id = Id });
+        }
+    }
+}
